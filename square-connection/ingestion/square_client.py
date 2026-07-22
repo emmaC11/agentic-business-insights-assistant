116f975140ec28caff492bae0231c5bfd76fa976
Emma C <c.emma2112@gmail.com>
2026-07-22 20:30:32 +0100

    fix typo on sales data model, add import & logging

--- square-connection/ingestion/square_client.py
+++ square-connection/ingestion/square_client.py
@@ -3,9 +3,12 @@ from square.environment import SquareEnvironment
 from square.core.api_error import ApiError
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
+from ingestion.sales_data_model import SalesRecord
 import os
+import logging
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 client = Square(
     environment=SquareEnvironment.PRODUCTION,
