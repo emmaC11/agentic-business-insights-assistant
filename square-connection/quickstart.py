c05325c20f5f4f7845fda9ee3a6013d2ea8978b4
Emma C <c.emma2112@gmail.com>
2026-07-22 09:27:10 +0100

    test fetching orders via orders API

--- square-connection/quickstart.py
+++ square-connection/quickstart.py
@@ -2,6 +2,7 @@ from square import Square
 from square.environment import SquareEnvironment
 from square.core.api_error import ApiError
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
 import os
 
 load_dotenv()
@@ -14,6 +15,7 @@ client = Square(
 try:
     response = client.locations.list()
     for location in response.locations:
+        loc_id = location.id
         print(f"{location.id}: ", end="")
         print(f"{location.name}, ", end="")
         print(f"{location.address}, ", end="")
@@ -45,4 +47,30 @@ except ApiError as e:
         print(error.category)
         print(error.code)
         print(error.detail)
+
+
+# 2 APIS required for forecasting model
+# 1 -> lListCatalog, acts as lookup to what items exist
+# 2 -> SearchOrders, every completed transaction, what was sold, how many etc, feeds forecasting model
+end_time = datetime.now(timezone.utc).isoformat()
+start_time = (datetime.now(timezone.utc) - timedelta(days=30)).isoformat()
+
+response = client.orders.search(
+    location_ids=[f"{loc_id}"],
+    query={
+        "filter": {
+            "state_filter": {"states": ["COMPLETED"]},
+            "date_time_filter": {
+                "closed_at": {"start_at": start_time, "end_at": end_time}
+            },
+        },
+    },
+    limit=10,
+)
+
+print(f'order count in period -> {len(response.orders)}')
+for order in response.orders:
+    print(f"closed: {order.closed_at}")
+    for li in order.line_items:
+        print(f"  {li.name} x {li.quantity} — catalog_id: {li.catalog_object_id} \n")
 
