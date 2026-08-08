eadf61b28614767497d092a4dcb10d0ad2e08bde
Emma C <c.emma2112@gmail.com>
2026-08-08 16:04:40 +0100

    trigger backfill from jun 2025 - jun 2026

--- square-connection/ingestion/runner.py
+++ square-connection/ingestion/runner.py
@@ -2,10 +2,12 @@ import os
 from dotenv import load_dotenv
 from square.environment import SquareEnvironment
 from ingestion.square_client import SquareIngestionClient
+from ingestion.storage import save_sales_records
 
 load_dotenv()
 
 token = os.environ['SQUARE_PROD_ACCESS_TOKEN']
+account_id = "TestBusinessAcc"
 
 client = SquareIngestionClient(
     token=token,
@@ -16,4 +18,7 @@ client = SquareIngestionClient(
 # client.catalog_lookup()
 # print("Done.")
 
-client.fetch_orders("2026-08-01T00:00:00Z", "2026-08-05T00:00:00Z")
+# client.fetch_orders("2026-08-01T00:00:00Z", "2026-08-05T00:00:00Z")
+records = client.fetch_orders("2025-06-01T00:00:00Z", "2026-06-30T00:00:00Z")
+path = save_sales_records(records, account_id=account_id)
+print(f"Data saved to: {path}")
