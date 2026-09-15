3d04f4f031f94ea1ab3303b72ba73cc4163232cb
Emma C <c.emma2112@gmail.com>
2026-09-15 20:52:54 +0100

    create baselines.py & add naive forecasting model

---
+++ models/baselines.py
@@ -0,0 +1,18 @@
+# forecasts / predicts using different models
+# every forecasting func follows the same signature
+# h-> horizon - number of weeks want to forecast
+import numpy as np
+
+def naive(train: np.ndarray, h: int) -> np.ndarray:
+    """forecast next n weeks, next week = last week"""
+
+    train = np.asarray(train)
+
+    if len(train) == 0:
+        raise ValueError("train np array empty")
+
+    if h <= 0:
+        raise ValueError("number of weeks to forecast / horizon must be greater than 0")
+
+    # n weeks will be last element of train array
+    return np.full(h, train[-1])
