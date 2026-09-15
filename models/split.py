d4a571ef96c931131378d7141a14d02e5a84d17c
Emma C <c.emma2112@gmail.com>
2026-09-15 19:48:58 +0100

    add split.py to create test & train numpy arrays

---
+++ models/split.py
@@ -0,0 +1,16 @@
+# split data into training data so model doesn't "cheat" by having access to full week data set
+# access to week 1-50, then predict 51-58
+import numpy as np
+
+TEST_WEEKS = 8 # no. of weeks that will be predicted
+
+def split_dataset(series, test_weeks=TEST_WEEKS):
+    # 1d num py array is expected input, but can accept list, series, array etc
+    series = np.asarray(series)
+
+
+    # split into train & test via slicing
+    train = series[:-test_weeks] # full range of data minus test_weeks
+    test = series[-test_weeks:] # last 8 weeks of data
+
+    return train, test 
