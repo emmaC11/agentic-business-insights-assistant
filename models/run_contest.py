7d445d8db2d831d4518c40af231651f019ec0bbf
Emma C <c.emma2112@gmail.com>
2026-09-16 20:15:29 +0100

    create run_contest.py & load weekly parquet as df

---
+++ models/run_contest.py
@@ -0,0 +1,14 @@
+# connects our 3 isolated components - metrics, split & baselines
+# uses our parquet & outputs analysis of models invoked and which one is best performing / wins "contest"
+
+import pandas as pd
+from pathlib import Path
+_REPO_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = _REPO_ROOT / "data"
+
+def model_analysis():
+    # load weekly parq as df
+    path = DATA_DIR / "TestBusinessAcc" / "sales_weekly.parquet" # replace hardcoded acc name
+    df = pd.read_parquet(path)
+
+
