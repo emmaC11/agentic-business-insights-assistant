69eaf09b126f1372b53eb5548437d976861b2621
Emma C <c.emma2112@gmail.com>
2026-09-19 19:08:44 +0100

    use lookup df to see item model performance

--- mcp-server/tools/forecast_demand.py
+++ mcp-server/tools/forecast_demand.py
@@ -13,4 +13,10 @@ def forecast_demand(df: pd.DataFrame, model_selection_df: pd.DataFrame, item: st
     tier = df[df["item"] == item]["tier"].iloc[0] # return raw string over pandas series
     if tier != "regular":
         return (f"item is not in regualr tier, only items in regualr tiers can be forecasted") # add tool to fetch regular tiered items?
+
+    # use lookup df to get model outputs for specific item
+    item_model_row = model_selection_df [model_selection_df["item"] == item]
+    model_name = item_model_row["model_row"]
+    mape = item_model_row["mape"]
+    mae = item_model_row["mae"]
     
