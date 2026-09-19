494f2e16c5c33c2fe0ece66aaee65841765ed510
Emma C <c.emma2112@gmail.com>
2026-09-19 19:20:08 +0100

    fix incorrect col name reference

--- mcp-server/tools/forecast_demand.py
+++ mcp-server/tools/forecast_demand.py
@@ -16,7 +16,7 @@ def forecast_demand(df: pd.DataFrame, model_selection_df: pd.DataFrame, item: st
 
     # use lookup df to get model outputs for specific item
     item_model_row = model_selection_df[model_selection_df["item"] == item]
-    model_name = item_model_row["model_row"]
+    model_name = item_model_row["model_winner"]
     mape = item_model_row["mape"]
     mae = item_model_row["mae"]
 
