9ca1321755560a9616f874b6218a4f5daa9e3502
Emma C <c.emma2112@gmail.com>
2026-09-19 18:57:03 +0100

    add conditionals to check h is valid & that items exists in df

--- mcp-server/tools/forecast_demand.py
+++ mcp-server/tools/forecast_demand.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+# forecast n weeks for item by using its best performing model (lookup df)
 def forecast_demand(df: pd.DataFrame, model_selection_df: pd.DataFrame, item: str, horizon_weeks: int):
+    if horizon_weeks <= 0 or horizon_weeks > 12:
+        raise ValueError("pred window must be greater than 0 and less than 12 weeks")
 
+    # check if item searched exists
+    if item not in df["item"].nunique():
+        return (f"item not found in sales history - {item}")
     
