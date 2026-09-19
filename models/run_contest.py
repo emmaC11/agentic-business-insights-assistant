74c4c3baa34a2a859a55c2a1e9ffe96cc417a4c2
Emma C <c.emma2112@gmail.com>
2026-09-19 13:09:35 +0100

    add MA model to model dict in run_contest.py

--- models/run_contest.py
+++ models/run_contest.py
@@ -4,15 +4,15 @@
 import pandas as pd
 from pathlib import Path
 from models.split import split_dataset, TEST_WEEKS
-from models.baselines import naive
+from models.baselines import naive, moving_average
 from models.metrics import mae_calc, mape_calc
 
 _REPO_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = _REPO_ROOT / "data"
 MODELS = {
     # model name + fn
-    "naive": naive
-    # add other models here when created
+    "naive": naive,
+    "moving_average":moving_average
 }
 
 def main():
