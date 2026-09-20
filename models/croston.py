f25c2a2e302edaf8a0d94dcf7ebf4c2687a49026
Emma C <c.emma2112@gmail.com>
2026-09-20 16:49:59 +0100

    create croston.py and add fn signature

---
+++ models/croston.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def croston(train: np.ndarray, h: int) -> np.ndarray:
+    """croston strong model choice for dataset with lots of 0s, designed specifically for items w intermittent demand""" 
